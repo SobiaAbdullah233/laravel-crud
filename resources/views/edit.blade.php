769db0ae230f78018f5e00ed416d4e527b6711e2
Sobia Abdullah <sobiaabdullah233@gmail.com>
2023-05-24 09:02:59 -0700

    test

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href=" {{asset('css/app.css')}}" type="text/css">
+    
     <style>
       html, body {
       min-height: 100%;
